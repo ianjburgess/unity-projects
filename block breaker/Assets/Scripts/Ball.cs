9494348d817f5ef99f59329950756d6a246f335f
ianjburgess <ianjbur@gmail.com>
2016-05-27 16:39:22 +1000

    added sound effect files and scripts

--- block breaker/Assets/Scripts/Ball.cs
+++ block breaker/Assets/Scripts/Ball.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class Ball : MonoBehaviour {
     private Paddle paddle;
     public Rigidbody2D rb;
-    private bool hasStarted = false;
 
+    private bool hasStarted = false;
     private Vector3 paddleToBallVector;
+    private AudioSource[] sounds;
+    private AudioSource bounce;
+    private AudioSource hit;
 
 	// Use this for initialization
 	void Start () {
@@ -14,17 +17,37 @@ public class Ball : MonoBehaviour {
         paddleToBallVector = this.transform.position - paddle.transform.position;
 
         rb = GetComponent<Rigidbody2D>();
+        sounds = GetComponents<AudioSource>();
+        bounce = sounds[0];
+        hit = sounds[1]; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (!hasStarted)    {
+            // Lock the ball relative to the paddle
             this.transform.position = paddle.transform.position + paddleToBallVector;
 
+            // Wait for a mouse press to launch
             if (Input.GetMouseButtonDown(0))    {
                 hasStarted = true;
                 rb.velocity = new Vector2(2f, 10f);
             }
         }
 	}
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        Vector2 tweak = new Vector2(Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
+
+        if(hasStarted) {
+            if(collision.gameObject.tag == "Breakable") {
+                hit.Play();
+                GetComponent<Rigidbody2D>().velocity += tweak;
+            } else {
+                bounce.Play();
+                GetComponent<Rigidbody2D>().velocity += tweak;
+            }
+        }
+    }
+
 }
--- block breaker/Assets/Scripts/Paddle.cs
+++ block breaker/Assets/Scripts/Paddle.cs
@@ -3,19 +3,34 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    public bool autoPlay = false;
+
+    private Ball ball;
+
+    void Start () {
+        ball = FindObjectOfType<Ball>();
+    }
+
 	// Update is called once per frame
 	void Update () {
-        Vector3 paddlePos = new Vector3(0.781f, this.transform.position.y, 0f);
-
-        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
-
-        paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.781f, 15.218f);
-
-        this.transform.position = paddlePos;
+        if (autoPlay == false) {
+            MoveWithMouse();
+        } else {
+            AutoPlay();
+        }
 	}
+
+    void AutoPlay() {
+        Vector3 paddlePos = new Vector3(0.781f, this.transform.position.y, 0f);
+        Vector3 ballPos = ball.transform.position;
+        paddlePos.x = Mathf.Clamp(ballPos.x, 0.781f, 15.218f);
+        this.transform.position = paddlePos;
+    }
+
+    void MoveWithMouse() {
+        Vector3 paddlePos = new Vector3(0.781f, this.transform.position.y, 0f);
+        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16;
+        paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.781f, 15.218f);
+        this.transform.position = paddlePos;
+    }
 }
