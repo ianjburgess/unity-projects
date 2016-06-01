38b48da145f1a36a016f569819adaa60e93fdcef
ianjburgess <ianjbur@gmail.com>
2016-06-01 11:48:10 +1000

    Clamp player movement to screen
    Used Camera.ViewportToWorldPoint() to work out the boundaries of the
    play space and bound the player within.

--- Lazer Defender/Assets/Scripts/PlayerController.cs
+++ Lazer Defender/Assets/Scripts/PlayerController.cs
@@ -4,23 +4,33 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     public float speed = 15.0f;
+    public float padding = 1f;
 
-    private Vector3 moveDirection = Vector3.zero;
-    // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
-            transform.position += new Vector3(+speed * Time.deltaTime, 0, 0);
-        } else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
-            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
-        }
+    float xmin;
+    float xmax;
+
+    void Start() {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmin = leftMost.x + padding;
+        xmax = rightMost.x - padding;
     }
 
-    void Controller() {
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
 
+        // Restrict Player to play space
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
     }
 }
+
+
+
+
