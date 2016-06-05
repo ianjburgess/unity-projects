8591f2b5ef56bcefae61d9aa178f65e643eb7ff9
ianjburgess <ianjbur@gmail.com>
2016-06-05 19:19:54 +1000

    Scripted enemy movement

--- Lazer Defender/Assets/Scripts/EnemySpawner.cs
+++ Lazer Defender/Assets/Scripts/EnemySpawner.cs
@@ -4,17 +4,47 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour {
 
     public GameObject enemyPrefab;
+    public float width = 10f;
+    public float height = 5f;
+
+    private bool movingRight = true;
+    public float speed = 15.0f;
+    public float padding = 1f;
+    private float xmin;
+    private float xmax;
 
 	// Use this for initialization
 	void Start () {
+        // Spawn movement boundary
+        float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftBoundary = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceToCamera));
+        Vector3 rightBoundary = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distanceToCamera));
+        xmax = rightBoundary.x;
+        xmin = leftBoundary.x;
+
+        // Set enemy to spawn point
         foreach (Transform child in transform) {
             GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
             enemy.transform.parent = child;
         }
 	}
+
+    public void OnDrawGizmos() {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (movingRight) {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        } else {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        float rightEdgeOfFormation = transform.position.x + (0.5f * width);
+        float leftEdgeOfFormation = transform.position.x - (0.5f * width);
+        if (leftEdgeOfFormation < xmin || rightEdgeOfFormation > xmax) {
+            movingRight = !movingRight;
+        }
+    }
 }
